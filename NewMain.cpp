4b1e068e238e4ee54745edbe4a31dad22cf95d85
ShalkYT <63145272+ShalkYT@users.noreply.github.com>
2025-08-25 17:02:28 +0000

    CreacionMain

---
+++ NewMain.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include "Genomas.h"
+#include "Secuencia.h"
+#include "GestorDeGenomas.h"
+
+void LimpiarPantalla();
+bool ArchivoExiste(std::string Nombre);
+void mostrarAyudaComando(std::string comando);
+void procesarComando(std::string comando,std::string parametros);
+
+int main(){
+    std::cout << "Hello World!!";
+}
